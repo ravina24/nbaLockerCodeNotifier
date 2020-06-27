0582b74a0c4a7d1f3e4f0027206176185acb768b
Protouch <protouch.tiles@gmail.com>
2020-06-27 11:24:21 -0700

    Script now working, still need to schedule

--- lockerCodeNotifier.py
+++ lockerCodeNotifier.py
@@ -1,64 +1,85 @@
-#Schedule every half hour using pythonanywhere
-
-# get json data from https://www.nba2k.io/nba-2k20-locker-codes/
-# get first locker code
-# compare id with previous first locker code
-# if same, end execution and wait until script runs again in half hour
-# if diff, send locker code to given phone numbers
-# save this new locker code as previous locker code
-
 import requests
 import json
+from twilio.rest import Client
+from datetime import datetime
+from creds import (account_sid, auth_token, my_phone_numbers, my_twilio_number)
 
-print "beginning of script"
 
 class LockerCode:
   def __init__(self, lockerCode, expiration):
     self.lockerCode = lockerCode
     self.expiration = expiration
 
-currentLockerCodeAmount = 124
 
-url = "https://www.nba2k.io/page-data/nba-2k20-locker-codes/page-data.json"
-response = requests.get(url)
+def getNewLockerCodes():
+	totalNumLockerCodesFile = open("totalNumLockerCodes.txt","r+")
+	currentLockerCodeAmount = int(totalNumLockerCodesFile.read())
 
-jsonResponse = response.json()
+	url = "https://www.nba2k.io/page-data/nba-2k20-locker-codes/page-data.json"
+	response = requests.get(url)
 
-prettyJson = json.loads(json.dumps(jsonResponse))
+	jsonResponse = response.json()
+	prettyJson = json.loads(json.dumps(jsonResponse))
+	lockerCodesJson = prettyJson["result"]["data"]["allLockerCodes"]["edges"]
+	lockerCodesJsonLength = len(lockerCodesJson)
 
-lockerCodesJson = prettyJson["result"]["data"]["allLockerCodes"]["edges"]
+	if lockerCodesJsonLength > currentLockerCodeAmount:
+		allNewLockerCodes = []
+		numNewLockerCodes = lockerCodesJsonLength - currentLockerCodeAmount;
+		index = 0;
 
-lockerCodesJsonLength = len(lockerCodesJson)
+		while numNewLockerCodes > 0:
+			newLockerCodeJson = lockerCodesJson[index]["node"]
+			newLockerCode = LockerCode(newLockerCodeJson["lockerCode"], newLockerCodeJson["expiration"])
+			# if newLockerCode.expiration > datetime.now(): DONT INCLUDE LOCKER CODES THAT HAVE ALREADY EXPIRED
+			allNewLockerCodes.append(newLockerCode)
+			index+=1
+			numNewLockerCodes-=1
 
-print("NUMBER OF LOCKER CODES")
-print (lockerCodesJsonLength)
+		
+		totalNumLockerCodesFile.truncate(0)
+		totalNumLockerCodesFile.seek(0)
+		totalNumLockerCodesFile.write(str(lockerCodesJsonLength))
+		totalNumLockerCodesFile.close()
 
-if lockerCodesJsonLength > currentLockerCodeAmount: #126 vs 124 -> 2
-	# send text to phone numbers with locker codes, time limits (check exp date first)
-	allNewLockerCodes = []
-	numNewLockerCodes = lockerCodesJsonLength - currentLockerCodeAmount;
-	index = 0;
+		return allNewLockerCodes
 
-	while numNewLockerCodes > 0:
-		newLockerCodeJson = lockerCodesJson[index]["node"]
-		newLockerCode = LockerCode(newLockerCodeJson["lockerCode"], newLockerCodeJson["expiration"])
-		allNewLockerCodes.append(newLockerCode)
-		index+=1
-		numNewLockerCodes-=1
-
-	#for code in allNewLockerCodes:
-		#print(code.lockerCode)
-
-	
-	# set currentLockerCodeAmount ENVIRONMENT VARIABLE to lockerCodesJsonLength, increment index, decrement numNewLockerCodes
-	currentLockerCodeAmount = lockerCodesJsonLength
-
-else:
-	# end script w/ no changes
-	print "end of script"
+	else:
+		return []
 
 
-#print(json.dumps(lockerCodesJson))
+def createText(lockerCodeArray):
+	text = "New locker codes available: \n"
+
+	for code in lockerCodeArray:
+		text += code.lockerCode + " expires " + code.expiration + "\n \n"
+
+	return text
+
+
+
+def send_lockercode_sms():
+
+    client = Client(account_sid, auth_token)
+    newLockerCodes = getNewLockerCodes()
+
+    if len(newLockerCodes) != 0:
+    	text = createText(newLockerCodes)
+    	print(text)
+
+    	# for phoneNumber in my_phone_numbers:
+
+	    # 	client.messages.create(
+	    #         from_=my_twilio_number,
+	    #         to=phoneNumber,
+	    #         body=text)
+    else:
+    	print "No new locker codes"
+    	
+
+if __name__ == '__main__':
+
+    send_lockercode_sms()
 
 
 
